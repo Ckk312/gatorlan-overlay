ca36493fc3ecec6f81d11fb31cc7728c25896d50
Ckk312 <17256501+Ckk312@users.noreply.github.com>
2025-01-31 15:46:08 -0500

    casters.ts added

---
+++ src/graphics/break/scripts/casters.ts
@@ -0,0 +1,8 @@
+import { Casters } from 'schemas';
+
+const casters = nodecg.Replicant<Casters>('casters');
+
+const castersWrapper = document.getElementById('info-bar-casters');
+const twittersWrapper = document.getElementById('info-bar-twitters');
+const castersLayout = document.getElementById('casters-layout');
+
