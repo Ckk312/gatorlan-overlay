d35ac5feedecf699f5ece652fc012417446f44c0
Ckk312 <17256501+Ckk312@users.noreply.github.com>
2025-02-12 03:11:41 -0500

    removed old scripts and prep for webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,10 @@
-
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import LiveReloadPlugin from 'webpack-livereload-plugin';
+import * as globby from 'globby';
+import * as path from 'path';
+import webpack from 'webpack';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 
 const entries = [];
 
@@ -7,3 +13,5 @@ let plugins = [];
 const config = {
     
 }
+
+export default [config];
